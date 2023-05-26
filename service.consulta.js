adf7bd5ea5ce80826c44df53fa8698835b7fcf23
FranXBL <francoxavierbonin@hotmail.com>
2023-05-26 13:08:05 -0300

    tarea-finalizada

--- service.consulta.js
+++ service.consulta.js
@@ -13,4 +13,29 @@ function consultaMotosDisponibles() {
   let motos = obtenerMotos();
   return motos;
 }
-module.exports = {serviceConsultaPrecios, consultaMotosDisponibles};
+
+function consultarMotoPorid(id){
+  let motos = obtenerMotos()
+  if(motos.length >=  id){
+    return motos[id]
+  }
+  return -1
+}
+
+function consultarPrecioPorIdMoto(id){
+  let precios = obtenerPrecios()
+  if(precios.length >=  id){
+    return precios[id]
+  }
+  return -1
+}
+
+function consultarMotoyPrecio(id){
+  let precios = obtenerPrecios()
+  let motos = obtenerMotos()
+  if(precios.length >= id){
+    return respuesta = {"Moto":motos[id], "Precios":precios[id]}
+  }
+  return -1
+}
+module.exports = {serviceConsultaPrecios, consultaMotosDisponibles, consultarMotoPorid, consultarPrecioPorIdMoto, consultarMotoyPrecio};
