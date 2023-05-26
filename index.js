e0806848e32025984aeafa58ce3cc08aaff70250
JuanHeis <juaany98@gmail.com>
2023-05-25 21:59:34 -0300

    tarea

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-const {serviceConsultaPrecios, consultaMotosDisponibles} = require("./service.consulta.js");
+const {
+  serviceConsultaPrecios,
+  consultaMotosDisponibles,
+} = require("./service.consulta.js");
 
 app.get("/api/consultaPrecios", (req, res) => {
   const data = serviceConsultaPrecios();
@@ -12,6 +15,13 @@ app.get("/api/motosDisponibles/", (req, res) => {
   const data = consultaMotosDisponibles();
   res.send(JSON.stringify(data));
 });
+
+// Tarea: Crear un endpoint que reciba un id de moto y devuelva el precio de la misma
+// Tarea: Crear un endpoint que reciba un id de moto y devuelva la moto
+// Tarea: Crear un endpoint que reciba un id de moto y devuelva la moto y el precio
+
+//pista para la tarea: https://expressjs.com/es/api.html#req.params
+//pista 2, los precios estan ordenados igual que las motos, por lo que el primer precio es el de la primera moto, el segundo precio es el de la segunda moto, etc.
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
