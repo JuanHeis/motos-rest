c41df32ffdf15d66128c9c86d4f5c0c91e74285c
JuanHeis <juaany98@gmail.com>
2023-05-25 22:00:40 -0300

    pr

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/api/motosDisponibles/", (req, res) => {
 // Tarea: Crear un endpoint que reciba un id de moto y devuelva el precio de la misma
 // Tarea: Crear un endpoint que reciba un id de moto y devuelva la moto
 // Tarea: Crear un endpoint que reciba un id de moto y devuelva la moto y el precio
-
+// Tarea: Mandarme un pull request con el codigo de la tarea
 //pista para la tarea: https://expressjs.com/es/api.html#req.params
 //pista 2, los precios estan ordenados igual que las motos, por lo que el primer precio es el de la primera moto, el segundo precio es el de la segunda moto, etc.
 app.listen(port, () => {
